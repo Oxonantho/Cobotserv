3855734d21a0fc26963759fa6d6a83ed644c290e
Anthony WENDLING - DRB Méthodes et Applications <anthony.wendling@drbmea.com>
2022-03-22 18:04:48 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
